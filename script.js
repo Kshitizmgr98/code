b0c832af7eb5d0b3c9cc382b16c1dc5ec4759142
Kshitiz Magar <kshitizmgr98@gmail.com>
2025-06-26 18:44:42 +0545

    something

--- script.js
+++ script.js
@@ -21,23 +21,46 @@
 // console.log(student.HMM)
 //  let arr1 = [1,2,3,4,5,"ram",]
 // console.log(arr1[0])
+//  we use if or else to make certain condition on java script like for eg
+// let x = 18
+// if (x >=18){
+//         console.log("you are not ready to be a world-class fighter")
+// }
+//         else if ( x >=10){
+//         console.log("go and practice some more kidd this is not some kindda joke")
+//         }
+//         else {
+    //         console.log("u have mostly what it take to ")
+      // }
 
 
+          // // for loop
+          // i++
+          // i--
+          // these are called increment oprater
+          // i++ is i=i+1
+          for(let i=-5;i<=5;i=i+1){
+            console.log("wtf are u",i)
+          }
 
 
-let students ={
-    student1:{ name: "Ram" ,age:16, hobby: "sleeping"},
-      student2:{ name: "Hari" ,age:17, hobby: "sleeping"},
-        student3:{ name: "Shyam" ,age:16, hobby: "sleeping"},
-          student4:{ name: "khari" ,age:16, hobby: "sleeping"},
-            student5:{ name: "kharki" ,age:17, hobby: "sleeping"},
-              student6:{ name: "Sunja" ,age:17, hobby: "sleeping"},
-                student7:{ name: "Sudan" ,age:17, hobby: "sleeping"},
-                  student8:{ name: "Sandip" ,age:17, hobby: "sleeping"},
-                    student9:{ name: "Suhant" ,age:17, hobby: "sleeping"},
-                      student10:{ name: "Danieal" ,age:17, hobby: "sleeping"},
-    
-}
-console.log("Those student name's who are really smart for their age are",students.student1.name)
-console.log("The student name's that are really smart are",students.student1.name)
-console.log("The student name's that are really smart are",students.student1.name)
+  
+  
+  
+
+let students = {
+              student1: { name: "Ram", age: 16, hobby: "sleeping" },
+                student2: { name: "Hari", age: 17, hobby: "sleeping" },
+                  student3: { name: "Shyam", age: 16, hobby: "sleeping" },
+                    student4: { name: "khari", age: 16, hobby: "sleeping" },
+                      student5: { name: "kharki", age: 17, hobby: "sleeping" },
+                        student6: { name: "Sunja", age: 17, hobby: "sleeping" },
+                          student7: { name: "Sudan", age: 17, hobby: "sleeping" },
+                            student8: { name: "Sandip", age: 17, hobby: "sleeping" },
+                              student9: { name: "Suhant", age: 17, hobby: "sleeping" },
+                                student10: { name: "Danieal", age: 17, hobby: "sleeping" },
+  }
+console.log("Those student name's who are really smart for their age are", students.student1.name)
+console.log("There  ", students.name)
+console.log("The student name's that are really smart are", students.name) 
+console.log("hello i am someone you are not ready to face as of now with your current power")
