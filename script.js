ccad9fabd0054bf6c6943839f16841f6b5cf1940
Kshitiz Magar <kshitizmgr98@gmail.com>
2025-06-26 15:57:37 +0545

    add some code

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 // for variable 
-let x = 2
-const y = 10
-let z = x + y
-console.log("the sum of this number is",z,)
-console.log(typeof x)
-console.log(2==2)
+// let x = 2
+// const y = 10
+// // let z = x + y
+// // console.log("the sum of this number is",z,)
+// // console.log(typeof x)
+// console.log(2==2)
+// // define a variables x and y 
 
 
+// console.log(2==4)
+// // type of data today arr1
+// // and object
+// let x = (2==4)
+// console.log(x)
+// let student = {
+//     name:"helo",
+//     age:"",
+//     HMM:"what",
+// }
+// console.log(student.HMM)
+//  let arr1 = [1,2,3,4,5,"ram",]
+// console.log(arr1[0])
+
+
+
+
+let students ={
+    student1:{ name: "Ram" ,age:16, hobby: "sleeping"},
+      student2:{ name: "Hari" ,age:17, hobby: "sleeping"},
+        student3:{ name: "Shyam" ,age:16, hobby: "sleeping"},
+          student4:{ name: "khari" ,age:16, hobby: "sleeping"},
+            student5:{ name: "kharki" ,age:17, hobby: "sleeping"},
+              student6:{ name: "Sunja" ,age:17, hobby: "sleeping"},
+                student7:{ name: "Sudan" ,age:17, hobby: "sleeping"},
+                  student8:{ name: "Sandip" ,age:17, hobby: "sleeping"},
+                    student9:{ name: "Suhant" ,age:17, hobby: "sleeping"},
+                      student10:{ name: "Danieal" ,age:17, hobby: "sleeping"},
+    
+}
+console.log("Those student name's who are really smart for their age are",students.student1.name)
+console.log("The student name's that are really smart are",students.student1.name)
+console.log("The student name's that are really smart are",students.student1.name)
